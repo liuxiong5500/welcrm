8b9a28b929465fae63c051ab9614f838421911f5
liu <liusrc@163.com>
2019-10-30 10:57:16 +0800

    revert

--- application/config/app-config.php
+++ application/config/app-config.php
@@ -56,11 +56,11 @@ define('APP_ENC_KEY','506b01a9c6db27d82606d7181701c052');
 //define('APP_DB_NAME','hook');
 
 /* The hostname of your database server. */
-define('APP_DB_HOSTNAME','localhost');
+define('APP_DB_HOSTNAME','49.232.149.19');
 /* The username used to connect to the database */
 define('APP_DB_USERNAME','root');
 /* The password used to connect to the database */
-define('APP_DB_PASSWORD','root');
+define('APP_DB_PASSWORD','Liuxiong544');
 /* The name of the database you want to connect to */
 define('APP_DB_NAME','hook');
 
